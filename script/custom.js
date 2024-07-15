3460471cac974fca67d9578be75bc47ffd41a0a7
rudeore-098 <rudeore0928@gmail.com>
2024-07-16 00:08:52 +0900

    STEW_Web_file

---
+++ script/custom.js
@@ -0,0 +1,18 @@
+$('.service-intro-page-btn').click(function(){
+    $('.tabs').css({'height': '300vh'})
+});
+
+$('.STEW-page-btn').click(function(){
+    $('.tabs').css({'height': '100vh'})
+});
+
+$('.find-page-btn').click(function(){
+    $('.tabs').css({'height': '100vh'})
+});
+$('.TEAM-page-btn').click(function(){
+    $('.tabs').css({'height': '100vh'})
+});
+
+// $('.service-intro-btn').click(function() {
+//     $('.tabs').toggleClass('expanded');
+// });
